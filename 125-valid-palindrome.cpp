b4986ab54c3c4f5b3a00e450e975284d39bb19a0
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-10-18 12:37:53 +0800

    update solutions

---
+++ 125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+bool isPalindrome(string s) {
+    int l = 0, r = s.size() - 1;
+    while (l < r) {
+        if (!isalnum(s[l])) {
+            ++l;
+            continue;
+        }
+        if (!isalnum(s[r])) {
+            --r;
+            continue;
+        }
+        if (tolower(s[l]) != tolower(s[r])) return false;
+        else {
+            ++l;
+            --r;
+        }
+    }
+    return true;
+}
