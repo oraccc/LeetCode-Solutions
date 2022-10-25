e7b6c2a512c425a7c7fd200571b7503997fbbe23
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-10-25 22:15:51 +0800

    update solutions

--- 1-100/47-permutation-II.cpp
+++ 1-100/47-permutation-II.cpp
@@ -19,3 +19,28 @@ void backtracking(vector<int> &nums, int pos, vector<vector<int>> &ans) {
         
     }
 }
+
+// Another solution: check before push answers (too much time cost)
+
+vector<vector<int>> permuteUnique(vector<int>& nums) {
+    vector<vector<int>> ans;
+    vector<int> currPerm;
+    backtracking(nums, 0, currPerm, ans);
+    return ans;
+}
+
+void backtracking(vector<int> &nums, int pos, vector<int> &currPerm, 
+    vector<vector<int>> &ans) {
+    if (pos == nums.size()) {
+        if (find(ans.begin(), ans.end(), currPerm) == ans.end())
+            ans.push_back(currPerm);
+        return;
+    }
+    for (int i = pos; i < nums.size(); ++i) {
+        swap(nums[i], nums[pos]);
+        currPerm.push_back(nums[pos]);
+        backtracking(nums, pos + 1, currPerm, ans);
+        currPerm.pop_back();
+        swap(nums[i], nums[pos]);   
+    }
+}
