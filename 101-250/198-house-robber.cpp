1f9c7e8e1567e7c295851c7506e3909a6a4536fc
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-02 22:54:20 +0800

    update solutions

---
+++ 101-250/198-house-robber.cpp
@@ -0,0 +1,11 @@
+int rob(vector<int>& nums) {
+    if (nums.empty()) return 0;
+    int n = nums.size();
+    if (n == 1) return nums[0];
+    vector<int> dp(n+1, 0);
+    dp[1] = nums[0];
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
+    }
+    return dp[n];
+}
