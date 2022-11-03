e28bf1331ef5c7683a56ffabbee19b46a3b3601f
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-03 22:22:47 +0800

    update solutions

---
+++ 101-250/221-maximal-square.cpp
@@ -0,0 +1,27 @@
+int maximalSquare(vector<vector<char>>& matrix) {
+    int m = matrix.size(), n = matrix[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    
+    int maxLength = 0;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (matrix[i][j] == '1') {
+                if (i == 0) {
+                    dp[i][j] = 1;
+                }
+                else if (j == 0) {
+                    dp[i][j] = 1;
+                }
+                else if (dp[i-1][j] == 0 || dp[i][j-1] == 0 || dp[i-1][j-1] == 0) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                }
+                maxLength = max(maxLength, dp[i][j]);
+            }
+        }
+    }
+
+    return maxLength * maxLength;
+}
