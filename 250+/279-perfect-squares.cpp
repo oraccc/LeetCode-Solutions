fa13863c8fca0067b60e9de344d762d198be3863
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-05 13:21:42 +0800

    update solutions

--- 250+/279-perfect-squares.cpp
+++ 250+/279-perfect-squares.cpp
@@ -1,3 +1,5 @@
+//dp
+
 int numSquares(int n) {
     vector<int> dp(n+1, INT_MAX);
     dp[0] = 0;
@@ -7,4 +9,31 @@ int numSquares(int n) {
         }
     }
     return dp[n];
+}
+
+//BFS
+
+int numSquares(int n) {
+    queue<int> q;
+    q.push(n);
+    int level = 0;
+
+    while (!q.empty()) {
+        ++level;
+        int qSize = q.size();
+        while (qSize--) {
+            int a = q.front();
+            q.pop();
+            for (int i = 1; i * i <= a; ++i) {
+                int b = a - i*i;
+                if (b == 0) {
+                    return level;
+                }
+                else {
+                    q.push(b);
+                }
+            }
+        }
+    }
+    return level;
 }
