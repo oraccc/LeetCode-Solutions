34eca1361bbcc38832777b3d112107cecb4ff7dc
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-08 22:58:43 +0800

    update solutions

---
+++ knapsack/0-1-knapsack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int knapsack(vector<int> weights, vector<int> values, int N, int W) {
+    vector<vector<int>> dp(N+1, vector<int>(W+1, 0));
+    for (int i = 1; i <= N; ++i) {
+        int w = weights[i-1], v = values[i-1];
+        for (int j = 1; j <= W; ++j) {
+            if (j >= w) {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v);
+            }
+            else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    return dp[N][W];
+}
+
+int knapsack2(vector<int> weights, vector<int> values, int N, int W) {
+    vector<int> dp(W+1, 0);
+    for (int i = 1; i <= N; ++i) {
+        int w = weights[i-1], v = values[i-1];
+        for (int j = W; j >= W; --j) {
+            dp[j] = max(dp[j], dp[j-w] + v);
+        }
+    }
+    return dp[W];
+}
