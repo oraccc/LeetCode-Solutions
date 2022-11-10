0dbe9510d6f8b76d92570fb7cacb2557d808eb66
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-10 22:17:50 +0800

    update solutions

---
+++ 250+/650-2-keys-keyboard.cpp
@@ -0,0 +1,14 @@
+int minSteps(int n) {
+    vector<int> dp(n+1, 0);
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = i;
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0) {
+                dp[i] = dp[j] + dp[i/j];
+                break;
+            }
+        }
+    }
+
+    return dp[n];
+}
