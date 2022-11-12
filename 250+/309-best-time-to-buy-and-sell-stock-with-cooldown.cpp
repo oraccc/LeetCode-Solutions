1d58c0618f1c0e32983e1fd872b73c305623359e
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-12 19:52:21 +0800

    update solutions

---
+++ 250+/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,18 @@
+int maxProfit(vector<int>& prices) {
+    int n =prices.size();
+    if (n == 1) return 0;
+    vector<int> buy(n+1, 0), sell(n+1, 0);
+    buy[0] = -prices[0];
+
+    for (int i = 1; i <= n; ++i) {
+        if (i == 1) {
+            buy[i] = max(buy[i-1], -prices[i-1]);
+        }
+        else {
+            buy[i] = max(buy[i-1], sell[i-2] - prices[i-1]);
+        }
+        sell[i] = max(sell[i-1], buy[i-1] + prices[i-1]);
+    }
+
+    return sell[n];
+}
