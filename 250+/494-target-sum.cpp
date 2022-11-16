08a030de1d652217373163bae03056734db53c6e
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-16 15:49:03 +0800

    update solutions

--- 250+/494-target-sum.cpp
+++ 250+/494-target-sum.cpp
@@ -1,15 +1,43 @@
+int count = 0;
 int findTargetSumWays(vector<int>& nums, int target) {
-    int currSum = 0, count = 0;
-    dfs(0, currSum, nums, target, count);
+    int currSum = 0;
+    dfs(0, currSum, nums, target);
     return count;
 }
 
-void dfs(int index, int currSum, vector<int> &nums, int target, int &count) {
+void dfs(int index, int currSum, vector<int> &nums, int target) {
     if (index == nums.size()) {
         if (currSum == target)
             ++count;
         return;
     }
-    dfs(index+1, currSum + nums[index], nums, target, count);
-    dfs(index+1, currSum -= nums[index], nums, target, count);
+    dfs(index+1, currSum + nums[index], nums, target);
+    dfs(index+1, currSum -= nums[index], nums, target);
+}
+
+//better time complexity
+int n;
+map<pair<int,int>,int> cache;
+
+int dfs(vector<int>& nums, int target, int currSum, int i)
+{
+    auto find = cache.find(make_pair(i, currSum));
+    
+    if (find != cache.end())
+        return find->second;
+    
+    if (i == nums.size() && currSum == target)
+        return 1;
+
+    if (i >= nums.size())
+        return 0;
+
+    int sum = dfs(nums, target, currSum + nums[i], i + 1) + dfs(nums, target, currSum - nums[i], i + 1);
+    cache.insert(make_pair(make_pair(i, currSum), sum));
+    return sum;
+}
+
+int findTargetSumWays(vector<int>& nums, int target) 
+{
+    return dfs(nums, target, 0, 0);
 }
