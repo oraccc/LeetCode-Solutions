799c62731339b969cbba68e9b900965ad276372b
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-16 23:27:44 +0800

    update solutions

---
+++ 101-250/241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,24 @@
+// divide and conquer
+vector<int> diffWaysToCompute(string expression) {
+    vector<int> result;
+    for (int i = 0; i < expression.size(); ++i) {
+        char c = expression[i];
+        if (c == '+' || c == '-' || c == '*') {
+            vector<int> left = diffWaysToCompute(expression.substr(0, i));
+            vector<int> right = diffWaysToCompute(expression.substr(i+1));
+            for (const int &l : left) {
+                for (const int &r : right) {
+                    switch(c) {
+                        case '+': result.push_back(l + r); break;
+                        case '-': result.push_back(l - r); break;
+                        case '*': result.push_back(l * r); break;
+                    }
+                }
+            }
+        }
+    }
+    if (result.empty()) {
+        result.push_back(stoi(expression));
+    }
+    return result;
+}
