542e29cf9167ba48e3d0007aded99d90938f69c5
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-11-21 14:00:50 +0800

    update solutions

---
+++ 101-250/224-basic-calculator.cpp
@@ -0,0 +1,38 @@
+int calculate(string s) {
+    stack<int> nums; 
+    int answer = 0, sign = 1; 
+    long long current_val = 0;
+    
+    for (const char &c : s) { 
+        if (isdigit(c)) {
+            current_val = 10 * current_val + c - '0'; 
+        }
+        else if(c == '+'){
+            answer += current_val * sign; 
+            current_val = 0;
+            sign = 1;   
+        }
+        else if(c == '-'){
+            answer += current_val * sign; 
+            current_val = 0; 
+            sign = -1; 
+        }
+        else if(c == '('){
+            nums.push(answer);    
+            nums.push(sign); 
+            answer = 0; 
+            sign = 1;
+        }
+        else if(c == ')'){ 
+            answer += current_val * sign; 
+            current_val = 0;
+            
+            answer = answer * nums.top();
+            nums.pop();
+            answer = answer + nums.top();
+            nums.pop();
+        }
+    }
+    answer += current_val * sign; 
+    return answer; 
+}
