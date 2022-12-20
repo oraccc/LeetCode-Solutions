659be71882776d525fcd633fd9536ee075fb3b2b
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-12-20 12:13:38 +0800

    update solutions

---
+++ 500+/528-random-pick-with-weight.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    vector<int> sums;
+public:
+    Solution(vector<int>& w) : sums(std::move(w)){
+        partial_sum(sums.begin(), sums.end(), sums.begin());
+        
+    }
+    
+    int pickIndex() {
+        int pos = (rand() % sums.back()) + 1;
+        return lower_bound(sums.begin(), sums.end(), pos) - sums.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
