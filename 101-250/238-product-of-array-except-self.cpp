64ba46b33c121f05939c428dff9e6d0fc87f05e9
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-12-21 11:51:01 +0800

    update solutions

---
+++ 101-250/238-product-of-array-except-self.cpp
@@ -0,0 +1,16 @@
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> left(n, 1), right(n, 1);
+    for (int i = 1; i < n; ++i) {
+        left[i] = left[i-1] * nums[i-1];
+    }
+    for (int j = n-2; j >= 0; --j) {
+        right[j] = right[j+1] * nums[j+1];
+    }
+
+    vector<int> ans(n, 1);
+    for (int k = 0; k < n; ++k) {
+        ans[k] = left[k] * right[k];
+    }
+    return ans;
+}
