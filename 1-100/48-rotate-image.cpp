42ac6c242feb4a79d459ffd2448cff614b319f55
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-01-17 16:05:47 +0800

    update solutions

---
+++ 1-100/48-rotate-image.cpp
@@ -0,0 +1,12 @@
+void rotate(vector<vector<int>>& matrix) {
+    int temp = 0, n = matrix.size()-1;
+    for (int i = 0; i <= n/2; ++i) {
+        for (int j = i; j < n-i; ++j) {
+            temp = matrix[i][j];
+            matrix[i][j] = matrix[n-j][i];
+            matrix[n-j][i] = matrix[n-i][n-j];
+            matrix[n-i][n-j] = matrix[j][n-i];
+            matrix[j][n-i] = temp;
+        }
+    }
+}
