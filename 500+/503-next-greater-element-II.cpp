5e114f184371301b80015b4004d18bd3620798bf
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-01-30 20:36:08 +0800

    update solutions

--- 500+/503-next-greater-element-II.cpp
+++ 500+/503-next-greater-element-II.cpp
@@ -1,23 +1,16 @@
 vector<int> nextGreaterElements(vector<int>& nums) {
-    unordered_map<int, int> next;
     stack<int> s;
     int n = nums.size();
-    for (int i = 0; i < n; ++i) {
-        next[i] = -1;
-    }
+    vector<int> ans(n, -1);
 
     for (int i = 0; i < 2*n; ++i) {
         int num = nums[i % n];
         while (!s.empty() && nums[s.top()] < num) {
             int pre = s.top();
             s.pop();
-            next[pre] = num;
+            ans[pre] = num;
         }
         s.push(i % n);
-    }
-    vector<int> ans;
-    for (int i = 0; i < n; ++i) {
-        ans.push_back(next[i]);
     }
     
     return ans;
