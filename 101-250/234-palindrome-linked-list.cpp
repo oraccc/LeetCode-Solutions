9332345f446ff00a729916910788ece7d7f66567
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-02-11 16:46:40 +0800

    update solutions

--- 101-250/234-palindrome-linked-list.cpp
+++ 101-250/234-palindrome-linked-list.cpp
@@ -19,3 +19,36 @@ bool isPalindrome(ListNode* head) {
 
     return s.empty();
 }
+
+//divide in half and reverse
+
+bool isPalindrome(ListNode* head) {
+    if (!head->next) return true;
+    ListNode *slow = head, *fast = head;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    slow->next = reverseList(slow->next);
+    slow = slow->next;
+    while (slow) {
+        if (head->val != slow->val) {
+            return false;
+        }
+        head = head->next;
+        slow = slow->next;
+    }
+    return true;
+
+}
+
+ListNode* reverseList(ListNode* head) {
+    ListNode *prev = nullptr, *next;
+    while (head) {
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
