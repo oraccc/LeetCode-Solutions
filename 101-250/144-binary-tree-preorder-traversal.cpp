a9b78e5a58f495e3af2d121bde9096793f2cda4a
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-02-28 10:27:18 +0800

    update solutions

--- 101-250/144-binary-tree-preorder-traversal.cpp
+++ 101-250/144-binary-tree-preorder-traversal.cpp
@@ -15,3 +15,23 @@ public:
         helper(root->right);
     }
 };
+
+//Iteratively Version
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (root == nullptr) return ans;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* n = s.top();
+            s.pop();
+            ans.push_back(n->val);
+            if (n->right) s.push(n->right);
+            if (n->left) s.push(n->left);
+        }
+        return ans;
+    }
+
+};
