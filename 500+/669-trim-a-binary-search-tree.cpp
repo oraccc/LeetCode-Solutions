e2a1f7e5dee7b8a16f28ded98a2b0e93ce7a4074
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-02-28 12:54:11 +0800

    update solutions

---
+++ 500+/669-trim-a-binary-search-tree.cpp
@@ -0,0 +1,10 @@
+TreeNode* trimBST(TreeNode* root, int low, int high) {
+    if (!root) return nullptr;
+    if (root->val > high)
+        return trimBST(root->left, low, high);
+    if (root->val < low)
+        return trimBST(root->right, low, high);
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    return root;
+}
