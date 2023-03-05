6eb1e7ab33b49309455ef9aa08a7538d24e50bee
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-03-05 22:13:51 +0800

    update solutions

---
+++ 201-300/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,8 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == nullptr) return nullptr;
+    if (root == p || root == q) return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if (left && right) return root;
+    else return left ? left : right;
+}
