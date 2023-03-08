3579b8c006a4915db60598cb863d4947cedef032
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-03-08 23:28:11 +0800

    update solutions

---
+++ 201-300/210-course-schedule-II.cpp
@@ -0,0 +1,35 @@
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> graph(numCourses, vector<int>());
+    vector<int> indegree(numCourses, 0);
+    vector<int> ans;
+    for (const auto& pair : prerequisites) {
+        graph[pair[1]].push_back(pair[0]);
+        ++indegree[pair[0]];
+    }
+
+    queue<int> q;
+    for (int i = 0; i < indegree.size(); ++i) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+    while (!q.empty()) {
+        int u = q.front();
+        ans.push_back(u);
+        q.pop();
+        for (auto v : graph[u]) {
+            --indegree[v];
+            if (indegree[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+
+    for (int i = 0; i < indegree.size(); ++i) {
+        if (indegree[i] != 0) {
+            return vector<int>();
+        }
+    }
+
+    return ans;
+}
