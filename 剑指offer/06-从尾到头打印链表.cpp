8187cf933f3b22f72e1b10fb1fc5c7ce23fe534b
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-05-10 20:03:11 +0800

    update solutions

---
+++ 剑指offer/06-从尾到头打印链表.cpp
@@ -0,0 +1,10 @@
+vector<int> reversePrint(ListNode* head) {
+    vector<int> ans;
+    while(head != nullptr) {
+        ans.push_back(head->val);
+        head = head->next;
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
