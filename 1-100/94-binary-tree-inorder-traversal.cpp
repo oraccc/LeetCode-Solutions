f9ebce56d34aed2734bdeb72797b1cbc1738ceee
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-05-18 21:08:16 +0800

    update solutions

--- 1-100/94-binary-tree-inorder-traversal.cpp
+++ 1-100/94-binary-tree-inorder-traversal.cpp
@@ -4,15 +4,10 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         if (root == nullptr) return ans;
-        helper(root);
-        return ans;
-    }
-
-    void helper(TreeNode* root) {
-        if (root == nullptr) return;
-        helper(root->left);
+        inorderTraversal(root->left);
         ans.push_back(root->val);
-        helper(root->right);
+        inorderTraversal(root->right);
+        return ans;
     }
 };
 
