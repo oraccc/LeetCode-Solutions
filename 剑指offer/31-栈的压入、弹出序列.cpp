2410b2edeba58cdc66b45458ce6aab1808ba7d97
Yang Xu <xuyang2018@sjtu.edu.cn>
2023-05-23 19:25:35 +0800

    update solutions

---
+++ 剑指offer/31-栈的压入、弹出序列.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> s;
+        for (int i = 0, j = 0; i < pushed.size(); ++i) {
+            s.push(pushed[i]);
+            while (j < popped.size() && !s.empty() && popped[j] == s.top()) {
+                s.pop();
+                ++j;
+            }
+        }
+        return s.empty();
+    }
+};
