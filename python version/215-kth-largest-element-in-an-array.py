ef6ab89f4f6a41e1f95eb760088e16c1a341bba8
oraccc <xuyang2018@sjtu.edu.cn>
2024-02-28 17:27:51 +0800

    update solutions

--- python version/215-kth-largest-element-in-an-array.py
+++ python version/215-kth-largest-element-in-an-array.py
@@ -8,3 +8,27 @@ class Solution:
             heapq.heappop(heap)
 
         return heap[0]
+    
+
+# quick_sort
+    
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quick_sort_k(nums, k):
+            pivot = random.choice(nums)
+            big, equal, small = [], [], []
+            for num in nums:
+                if num > pivot:
+                    big.append(num)
+                elif num < pivot:
+                    small.append(num)
+                else:
+                    equal.append(num)
+
+            if k <= len(big):
+                return quick_sort_k(big, k)
+            if k > len(big) + len(equal):
+                return quick_sort_k(small, k-len(big)-len(equal))
+
+            return pivot
+        return quick_sort_k(nums, k)
